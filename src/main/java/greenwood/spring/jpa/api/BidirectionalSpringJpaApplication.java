3c3a6e5b5144d802e87265376059e2e825c7e218
Ferysuryanta <ferysuryanta01@gmail.com>
2023-06-06 20:42:56 +0700

    first-commit

---
+++ src/main/java/greenwood/spring/jpa/api/BidirectionalSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package greenwood.spring.jpa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidirectionalSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidirectionalSpringJpaApplication.class, args);
+	}
+
+}
